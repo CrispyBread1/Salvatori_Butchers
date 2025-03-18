e6081127bf1927c9473e45d0df5bce6caf99b063
CrispyBread1 <dsalvatori2008@gmail.com>
2025-03-18 12:44:20 +0000

    Stock take products get retreived and in objects and sorted

--- database/products.py
+++ database/products.py
@@ -1,5 +1,7 @@
 import psycopg2
 from psycopg2 import sql
+from models.product import Product
+
 
 
 DB_HOST='aws-0-eu-west-2.pooler.supabase.com'
@@ -9,72 +11,84 @@ DB_USER='postgres.qebranyoffuyctlpemxx'
 DB_PASSWORD='KQz9Q0PP8vjCpaDR'
 
 def connect_db():
-    try:
-        # Establish the connection
-        connection = psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD
-        )
-        print("Connection to database successful")
-        return connection
-    except Exception as e:
-        print(f"Failed to connect to database: {e}")
-        return None
+  try:
+      # Establish the connection
+      connection = psycopg2.connect(
+          host=DB_HOST,
+          port=DB_PORT,
+          dbname=DB_NAME,
+          user=DB_USER,
+          password=DB_PASSWORD
+      )
+      print("Connection to database successful")
+      return connection
+  except Exception as e:
+      print(f"Failed to connect to database: {e}")
+      return None
 
 
 def create_product_table():
-    connection = connect_db()
-    if connection:
-        cursor = connection.cursor()
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS products (
-                id SERIAL PRIMARY KEY,
-                name TEXT NOT NULL,
-                cost REAL,
-                stock_count REAL,
-                product_value REAL,
-                stock_category TEXT NOT NULL,
-                product_category TEXT NOT NULL,
-                sage_code TEXT,
-                supplier TEXT,
-                sold_as TEXT NOT NULL
-            )
-        """)
-        connection.commit()
-        print("Table 'products' created successfully.")
-        cursor.close()
-        connection.close()
+  connection = connect_db()
+  if connection:
+      cursor = connection.cursor()
+      cursor.execute("""
+          CREATE TABLE IF NOT EXISTS products (
+              id SERIAL PRIMARY KEY,
+              name TEXT NOT NULL,
+              cost REAL,
+              stock_count REAL,
+              product_value REAL,
+              stock_category TEXT NOT NULL,
+              product_category TEXT NOT NULL,
+              sage_code TEXT,
+              supplier TEXT,
+              sold_as TEXT NOT NULL
+          )
+      """)
+      connection.commit()
+      print("Table 'products' created successfully.")
+      cursor.close()
+      connection.close()
 
 def insert_product(name, cost, stock_count, product_value, stock_category, product_category, sage_code, supplier, sold_as):
-    connection = connect_db()
-    if connection:
-        cursor = connection.cursor()
-        cursor.execute("""
-            INSERT INTO products (name, cost, stock_count, product_value, stock_category, product_category, sage_code, supplier, sold_as) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """, (name, cost, stock_count, product_value, stock_category, product_category, sage_code, supplier, sold_as))
-        connection.commit()
-        print(f"Product {name} added successfully!")
-        cursor.close()
-        connection.close()
+  connection = connect_db()
+  if connection:
+      cursor = connection.cursor()
+      cursor.execute("""
+          INSERT INTO products (name, cost, stock_count, product_value, stock_category, product_category, sage_code, supplier, sold_as) 
+          VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+      """, (name, cost, stock_count, product_value, stock_category, product_category, sage_code, supplier, sold_as))
+      connection.commit()
+      print(f"Product {name} added successfully!")
+      cursor.close()
+      connection.close()
 
 def fetch_products():
-    connection = connect_db()
-    rows = []
-    if connection:
+  connection = connect_db()
+  rows = []
+  if connection:
+      cursor = connection.cursor()
+      cursor.execute("SELECT * FROM products")
+      rows = cursor.fetchall()
+      cursor.close()
+      connection.close()
+      return rows
+    
+def fetch_products_stock_take(categories):
+  connection = connect_db()
+  results = {}
+  if connection:
+      for category in categories:
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM products")
-        rows = cursor.fetchall()
+        cursor.execute(f"SELECT * FROM products WHERE stock_category = '{category}' ")
+        results[category] = convert_to_product_objects(cursor.fetchall())
         cursor.close()
         connection.close()
-        return rows
-    
-        
-
+        return results
+      
+def convert_to_product_objects(products):
+  return [Product(*product) for product in products]
 if __name__ == "__main__":
-    create_product_table()
+  create_product_table()
     
 
--- gui/stock_take_window.py
+++ gui/stock_take_window.py
@@ -1,8 +1,13 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QTableWidget, QTableWidgetItem, QPushButton
 from models.product import Product
-from database.products import fetch_products
+from database.products import fetch_products_stock_take
 
 class StockTakeWindow(QWidget):
-   def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Stock Take Window')
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle('Stock Take Window')
+    self.load_data()
+
+  def load_data(self):
+    data = fetch_products_stock_take(['fresh', 'dry', 'frozen'])
+    print(data)
