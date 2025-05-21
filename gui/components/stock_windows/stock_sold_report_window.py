a252d159435b0d180c03cee7bf318933f4b628bc
CrispyBread1 <dsalvatori2008@gmail.com>
2025-05-21 15:32:41 +0100

    Stock sold report, added date

--- gui/components/stock_windows/stock_sold_report_window.py
+++ gui/components/stock_windows/stock_sold_report_window.py
@@ -1,3 +1,4 @@
+from datetime import date
 from PyQt5.QtWidgets import (
     QWidget, QPushButton, QLabel, QStackedWidget,
     QVBoxLayout, QFrame, QHBoxLayout, QMainWindow, QDialog
@@ -13,10 +14,16 @@ class StockSoldReportWindow(QWidget):
 
   def __init__(self):
     super().__init__()
+    self.main_layout = QVBoxLayout()
+    self.title_label = QLabel()
+    self.title_label.setStyleSheet("font-size: 18px; font-weight: bold; margin-bottom: 15px;")
+    self.main_layout.addWidget(self.title_label)
     self.setup_ui()
         
-  def setup_ui(self):
-      layout = QVBoxLayout()
+  def setup_ui(self):      
+      self.date = date.today().strftime('%Y-%m-%d')
+      self.title_label.setText(f"Report for: {self.date}")
+
       
       # Title
       # title = QLabel("Stock Sold Report")
@@ -25,6 +32,8 @@ class StockSoldReportWindow(QWidget):
 
       self.change_date_button = QPushButton("Change Date", self)
       self.change_date_button.clicked.connect(self.change_date)
+      self.create_report_button = QPushButton("Create Report", self)
+      self.create_report_button.clicked.connect(self.create_report)
       self.add_product_button = QPushButton("Add product", self)
       self.add_product_button.clicked.connect(self.open_product_selection)
       self.remove_product_button = QPushButton("Remove product", self)
@@ -32,13 +41,14 @@ class StockSoldReportWindow(QWidget):
       
       self.button_layout = QHBoxLayout()
       self.button_layout.addWidget(self.change_date_button)
+      self.button_layout.addWidget(self.create_report_button)
       self.button_layout.addWidget(self.add_product_button)
       self.button_layout.addWidget(self.remove_product_button)
 
 
-      layout.addLayout(self.button_layout)
+      self.main_layout.addLayout(self.button_layout)
     
-      self.setLayout(layout)
+      self.setLayout(self.main_layout)
 
 
   def change_date(self):
@@ -66,4 +76,7 @@ class StockSoldReportWindow(QWidget):
       return None
   
   def remove_product(self):
-     pass
+      pass
+  
+  def create_report(self):
+      pass
